cf1a02d6c5b47eaf617f32a1b626d82c4f4836da
VIPIN <96302356+VipinGirjapure@users.noreply.github.com>
2022-04-02 00:05:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+ function navBar(){
+    var a = document.getElementById("right");
+    console.log(a)
+    if( a.style.display === "block"){
+        a.style.display ="none";
+    }
+    else{
+        a.style.display = "block";
+    }
+}
